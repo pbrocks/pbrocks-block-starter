b8700b086770f640dfc148e4dd309cad993f8c4b
Paul Barthmaier <pbrocks@users.noreply.github.com>
2022-02-16 16:32:51 -0500

    Delete move-slider-bar.js

--- inc/js/move-slider-bar.js
+++
@@ -1,9 +0,0 @@
-(function() {
-    let i = document.querySelector('#swiper__divider'),
-        o = document.querySelector('#slider');
-
-
-    o.addEventListener('change', function() {
-		i.style.width = o.value+"%";
-    }, false);
-})();
